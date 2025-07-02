4edae77ae4c4a89b855d141791a803d699bbf568
fengjujn-zizi <feng2131545912@icloud.com>
2025-07-02 17:18:35 +0700

    feature: add some feature

--- main.py
+++ main.py
@@ -1,21 +1,43 @@
-
 import mysql.connector
-import psycopg2
-import sqlite3
+
 class Main:
-    """main程序"""
-    mydb = mysql.connector.connect(
-        host = "localhost",
-        user = "root" ,
-        passwd = "",
-        database = "blood_alcohol"
-    )
+    def __init__(self):
+        self.r_boy = 0.68
+        self.r_girl = 0.55
+        self.p = 0.789
+        self.get_wine_name = ""
+        self.volume = 0
+        self.abv = 0
+        self.weight = 0
+        self.mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="",  # 如果你有密码，请填入
+            database="blood_alcohol"
+        )
 
-    mycursor = mydb.cursor()
-    mycursor.execute("CREATE TABLE IF NOT EXISTS blood_alcohol_content")
-    results = mycursor.fetchall()
+        self.cursor = self.mydb.cursor()
 
-    for row in results :
-        print(row)
+    def show_data(self):
+        self.cursor.execute("SELECT * FROM bac_levels")
+        rows = self.cursor.fetchall()
+        for row in rows:
+            print(row) #可以直接使用print函数来打印，但是使用for函数，比较美观
 
-    mydb.close()
+    def close_database(self):
+        self.cursor.close()
+        self.mydb.close()
+
+    def get_wine_name(self):
+        wine_name = input("Please enter the name of the wine: ")
+        volume = input("Please enter the volume of the wine: ")
+        abv = input("Please enter the fraction of the wine: ")
+        weight = input("Please enter the weight of the wine: ")
+        pr(f"Your the wine name is "wine_name, volume, abv, weight)
+
+    def calculate(self):
+
+
+# 运行
+app = Main()
+app.show_data()
